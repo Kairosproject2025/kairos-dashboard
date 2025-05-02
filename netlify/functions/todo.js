7b0a685a006db45bcfdf848f6ade2a6ce9bf2695
Kairosproject2025 <alberto_frasson@libero.it>
2025-05-02 14:18:34 +0200

    Create todo.js

---
+++ netlify/functions/todo.js
@@ -0,0 +1,6 @@
+export default async (req, res) => {
+  return res.json({
+    message: "TODO eseguito. Nessuna nuova istruzione operativa trovata. In attesa di segnale diretto."
+  });
+};
+Add todo.js function
