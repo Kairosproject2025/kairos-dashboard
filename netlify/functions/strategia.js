5253ebf11b27a5c1e10bd20d1802707e4c3d7df7
Kairosproject2025 <alberto_frasson@libero.it>
2025-05-02 14:30:51 +0200

    Create strategia.js

---
+++ netlify/functions/strategia.js
@@ -0,0 +1,10 @@
+export default async (req, res) => {
+  return res.json({
+    orizzonte: "Maggio 2025",
+    obiettivo: "+2.50% netto con drawdown max -2%",
+    nota: "Priorità su Sigma Watch, Score ETF e rotazione Alpha",
+    liquidità: "Sotto soglia operativa, valutare rotazione"
+  });
+};
+
+Add strategia.js function
