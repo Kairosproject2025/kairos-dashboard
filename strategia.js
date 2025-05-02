0c077975fcc82f6db367afdfd18314ef610757b6
Kairosproject2025 <alberto_frasson@libero.it>
2025-05-02 14:07:56 +0200

    Initial deploy - Kairos Dashboard

---
+++ strategia.js
@@ -0,0 +1,10 @@
+
+exports.handler = async function (event, context) {
+  return {
+    statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*"
+    },
+    body: JSON.stringify({ message: "STRATEGIA attivato da Kairos" })
+  };
+};
