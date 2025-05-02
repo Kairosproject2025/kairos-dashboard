95cbe90322bac294ecf2a119e767257765fbc12b
Kairosproject2025 <alberto_frasson@libero.it>
2025-05-02 14:24:14 +0200

    Create masterkey.js

---
+++ netlify/functions/masterkey.js
@@ -0,0 +1,10 @@
+export default async (req, res) => {
+  return res.json({
+    disciplina: "Attiva",
+    drawdown: "-0.57%",
+    netto: "+1.43%",
+    liquidità: "5.93%",
+    priorità: "Sbloccare COP e Radar esteso"
+  });
+};
+Add masterkey.js function
