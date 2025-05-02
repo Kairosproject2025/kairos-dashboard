faaebcf335500ee65664ddcbd82f70fe85cecac6
Kairosproject2025 <alberto_frasson@libero.it>
2025-05-02 16:46:36 +0200

    Update and rename aprimi-gli-occhi.js to

--- aprimi-gli-occhi.js
+++ aprimi-gli-occhi.js
@@ -1,10 +1,1 @@
 
-exports.handler = async function (event, context) {
-  return {
-    statusCode: 200,
-    headers: {
-      "Access-Control-Allow-Origin": "*"
-    },
-    body: JSON.stringify({ message: "APRIMI-GLI-OCCHI attivato da Kairos" })
-  };
-};
